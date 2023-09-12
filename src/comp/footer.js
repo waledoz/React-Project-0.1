530bcb819c4acb9582f196572cfff7647460864d
waledoz <waledoz@hotmail.com>
2023-09-12 22:47:00 +0300

    add comp

---
+++ src/comp/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        Designed and developed by Waledoz
+        <span>🧡</span>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
